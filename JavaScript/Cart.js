7b6c529aeb79995feb183b97a5fd5aa04224d767
Abdulrahman <abood20000339@gmail.com>
2025-04-08 01:53:14 +0300

    removed classes

--- JavaScript/Cart.js
+++ JavaScript/Cart.js
@@ -1,51 +1,92 @@
-const cart=document.querySelector(".js-Cart");
-const productsHTML=document.querySelector(".js-products");
-class Product{
-  id;
-  name;
-  description;
-  price;
-  image;
-  constructor(id,name,description,price,image){
-    this.id=id;
-    this.name=name;
-    this.description=description;
-    this.price=price;
-    this.image=image;
-  };
-}
-const products=[
-  new Product(1,"Amazon Astro","Household robot for home monitoring, with Alexa.",20.99,"../images/RobotProduct.jpeg"),
-
-  new Product(2,"Echo & Alexa Devices","Play music, control your smart home, get information, and more using just your voice.",17.45,"../images/alexaDevice.jpg"),
-
-  new Product(3,"Smart Home","Create a smart and connected home with Alexa and products from Amazon.",14.13,"../images/tabletProduct.jpg"),
-
-  new Product(4,"Amazon Kids","Devices and content designed just for kids to safely learn, grow and explore.",30.87,"../images/kidsProduct.png"), 
-  
-  new Product(5,"Kindle E-readers","The best devices for reading and writing, period. Hold thousands of books with no screen glare.",25.32,"../images/notbookProduct.png"),
-
-  new Product(6,"Home Security from Amazon","Protect your entire home with security systems and cameras from Amazon.",32.12,"../images/speakerProduct.jpeg"),
-
-  new Product(7,"Fire TV","Stream over 1.5 Million TV episodes and movies. Watch favorites from Netflix, Prime Video, Disney+, Max , and more.",57.45,"../images/fireTv.png"),
-  
-  new Product(8,"Fire Tablets","Powerful tablets designed for entertainment—at an affordable price.",21.75,"../images/tablets.jpeg"),
-   
-  new Product(9,"Amazon Luna","Play games like you stream movies. Luna is a cloud gaming service that lets you play games on devices you already own.",56.39,"../images/controller.jpeg"),
+const cart = document.querySelector(".js-Cart");
+const productsHTML = document.querySelector(".js-products");
+const products = [
+  {
+    id: 1,
+    name: "Amazon Astro",
+    description: "Household robot for home monitoring, with Alexa.",
+    price: 20.99,
+    image: "../images/RobotProduct.jpeg",
+  },
+  {
+    id: 2,
+    name: "Echo & Alexa Devices",
+    description:
+      "Play music, control your smart home, get information, and more using just your voice.",
+    price: 17.45,
+    image: "../images/alexaDevice.jpg",
+  },
+  {
+    id: 3,
+    name: "Smart Home",
+    description:
+      "Create a smart and connected home with Alexa and products from Amazon.",
+    price: 14.13,
+    image: "../images/tabletProduct.jpg",
+  },
+  {
+    id: 4,
+    name: "Amazon Kids",
+    description:
+      "Devices and content designed just for kids to safely learn, grow and explore.",
+    price: 30.87,
+    image: "../images/kidsProduct.png",
+  },
+  {
+    id: 5,
+    name: "Kindle E-readers",
+    description:
+      "The best devices for reading and writing, period. Hold thousands of books with no screen glare.",
+    price: 25.32,
+    image: "../images/notbookProduct.png",
+  },
+  {
+    id: 6,
+    name: "Home Security from Amazon",
+    description:
+      "Protect your entire home with security systems and cameras from Amazon.",
+    price: 32.12,
+    image: "../images/speakerProduct.jpeg",
+  },
+  {
+    id: 7,
+    name: "Fire TV",
+    description:
+      "Stream over 1.5 Million TV episodes and movies. Watch favorites from Netflix, Prime Video, Disney+, Max , and more.",
+    price: 57.45,
+    image: "../images/fireTv.png",
+  },
+  {
+    id: 8,
+    name: "Fire Tablets",
+    description:
+      "Powerful tablets designed for entertainment—at an affordable price.",
+    price: 21.75,
+    image: "../images/tablets.jpeg",
+  },
+  {
+    id: 9,
+    name: "Amazon Luna",
+    description:
+      "Play games like you stream movies. Luna is a cloud gaming service that lets you play games on devices you already own.",
+    price: 56.39,
+    image: "../images/controller.jpeg",
+  },
 ];
-const extraItems=JSON.parse(localStorage.getItem("extraItems"))||products.slice();
-const checkoutItems=JSON.parse(localStorage.getItem("cartItems"))||[];
-const cartProducts=cartItems;
-let extraProducts=extraItems;
-let totalPrice=0.00;
-let totalItem=0;
-let filteredProducts=extraProducts.slice();
-function updateCartPro(){
-  totalPrice=0.00;
-  totalItem=0;
-  let cartProductHTML="";
-  cartProducts.forEach((cProduct,index)=>{
-    cartProductHTML+=`
+const extraItems =
+  JSON.parse(localStorage.getItem("extraItems")) || products.slice();
+const checkoutItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+const cartProducts = cartItems;
+let extraProducts = extraItems;
+let totalPrice = 0.0;
+let totalItem = 0;
+let filteredProducts = extraProducts.slice();
+function updateCartPro() {
+  totalPrice = 0.0;
+  totalItem = 0;
+  let cartProductHTML = "";
+  cartProducts.forEach((cProduct, index) => {
+    cartProductHTML += `
     <div class="CartItem">
       <div id="cartImgCon">
         <img src="${cProduct.image}" alt="">
@@ -70,52 +111,54 @@ function updateCartPro(){
     </div>
   </div>
 </div>`;
-totalPrice=((cProduct.price*100)+(totalPrice*100))/100;
-totalItem+=1;
-totalHTML.forEach((total,index)=>{
-  if(totalItem==1){
-    total.innerHTML=`Subtotal (${totalItem} item): <b class="red">$${totalPrice.toFixed(2)}</b>`
+    totalPrice = (cProduct.price * 100 + totalPrice * 100) / 100;
+    totalItem += 1;
+    totalHTML.forEach((total, index) => {
+      if (totalItem == 1) {
+        total.innerHTML = `Subtotal (${totalItem} item): <b class="red">$${totalPrice.toFixed(
+          2
+        )}</b>`;
+      } else {
+        total.innerHTML = `Subtotal (${totalItem} items): <b class="red">$${totalPrice.toFixed(
+          2
+        )}</b>`;
+      }
+    });
+  });
+  totalHTML.forEach((total, index) => {
+    if (totalItem == 1) {
+      total.innerHTML = `Subtotal (${totalItem} item): <b class="red">$${totalPrice.toFixed(
+        2
+      )}</b>`;
+    } else {
+      total.innerHTML = `Subtotal (${totalItem} items): <b  class="red">$${totalPrice.toFixed(
+        2
+      )}</b>`;
     }
-    else{
-       total.innerHTML=`Subtotal (${totalItem} items): <b class="red">$${totalPrice.toFixed(2)}</b>`
-    }
+  });
+  localStorage.setItem("extraItems", JSON.stringify(extraProducts));
+  localStorage.setItem("cartItems", JSON.stringify(cartProducts));
+  cart.innerHTML = cartProductHTML;
+  const removeBtnHtmL = document.querySelectorAll(".js-RemoveButton");
+  function updateButtons() {
+    removeBtnHtmL.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        extraProducts.push(cartProducts[index]);
+        filteredProducts = extraProducts;
+        updateFilteredProducts();
+        cartProducts.splice(index, 1);
+        updateCartPro();
+        updateExtraPro();
+        updateNumber();
+      });
+    });
   }
-)
-  }
-)
-totalHTML.forEach((total,index)=>{
-  if(totalItem==1){
-    total.innerHTML=`Subtotal (${totalItem} item): <b class="red">$${totalPrice.toFixed(2)}</b>`
-    }
-    else{
-       total.innerHTML=`Subtotal (${totalItem} items): <b  class="red">$${totalPrice.toFixed(2)}</b>`
-    }
-  }
-)
-localStorage.setItem("extraItems",JSON.stringify(extraProducts));
-localStorage.setItem("cartItems",JSON.stringify(cartProducts));
-cart.innerHTML=cartProductHTML;
-const removeBtnHtmL=document.querySelectorAll(".js-RemoveButton");
-function updateButtons(){
-removeBtnHtmL.forEach((button,index)=>{
-   button.addEventListener("click",()=>{
-      extraProducts.push(cartProducts[index]);
-      filteredProducts=extraProducts;
-      updateFilteredProducts();
-      cartProducts.splice(index,1);
-      updateCartPro();
-      updateExtraPro();
-      updateNumber();
-   }
-  )
-});
+  setInterval(updateButtons(), 100);
 }
-setInterval(updateButtons(),100);
-};
-function updateExtraPro(){
-  let extraProductsHTML="";
-  filteredProducts.forEach((eProduct,index)=>{
-  extraProductsHTML+=`
+function updateExtraPro() {
+  let extraProductsHTML = "";
+  filteredProducts.forEach((eProduct, index) => {
+    extraProductsHTML += `
 <div class="js-Product" id="product"> 
  <a href="#">
     <img src="${eProduct.image}" alt="">
@@ -135,62 +178,62 @@ function updateExtraPro(){
   </button>
 </div> 
 </div>
-`; 
-}
-)
-localStorage.setItem("extraItems",JSON.stringify(extraProducts));
-localStorage.setItem("cartItems",JSON.stringify(cartProducts));
-productsHTML.innerHTML=extraProductsHTML;
-const addToCart=document.querySelectorAll(".js-cartButton");
-function updateButtons1(){
-addToCart.forEach((button,index)=>{
-  button.addEventListener("click",()=>{
-    cartProducts.push(filteredProducts[index]);
-    filteredProducts.splice(index,1);
-    let include=false;
-    extraProducts=products.filter((product)=>{
-      include=false;
-      cartProducts.forEach((cProduct)=>{
-        if(cProduct.id===product.id){
-          include=true;
-        }
+`;
+  });
+  localStorage.setItem("extraItems", JSON.stringify(extraProducts));
+  localStorage.setItem("cartItems", JSON.stringify(cartProducts));
+  productsHTML.innerHTML = extraProductsHTML;
+  const addToCart = document.querySelectorAll(".js-cartButton");
+  function updateButtons1() {
+    addToCart.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        cartProducts.push(filteredProducts[index]);
+        filteredProducts.splice(index, 1);
+        let include = false;
+        extraProducts = products.filter((product) => {
+          include = false;
+          cartProducts.forEach((cProduct) => {
+            if (cProduct.id === product.id) {
+              include = true;
+            }
+          });
+          if (!include) {
+            return product;
+          }
+        });
+        updateExtraPro();
+        updateCartPro();
+        updateNumber();
       });
-      if(!include){
-        return product;
-      }
     });
-    updateExtraPro();
-    updateCartPro();
-    updateNumber();
-    }
-  );
-});
+  }
+  setInterval(updateButtons1(), 100);
 }
-setInterval(updateButtons1(),100);
-};
-const totalHTML=document.querySelectorAll(".js-Total");
-const ItemsInCart=document.querySelector(".js-NumberCart");
+const totalHTML = document.querySelectorAll(".js-Total");
+const ItemsInCart = document.querySelector(".js-NumberCart");
 function updateNumber() {
-  ItemsInCart.innerHTML=cartProducts.length;  
+  ItemsInCart.innerHTML = cartProducts.length;
 }
-function updateFilteredProducts(){
-  filteredProducts=extraProducts.filter((eProduct)=>{
-    if(eProduct.name.toLowerCase().includes(searchBar.value.trim().toLowerCase())){
+function updateFilteredProducts() {
+  filteredProducts = extraProducts.filter((eProduct) => {
+    if (
+      eProduct.name.toLowerCase().includes(searchBar.value.trim().toLowerCase())
+    ) {
       return eProduct;
     }
-  })
+  });
 }
-  searchBar.addEventListener("input",()=>{
-    updateFilteredProducts();
-    updateExtraPro();
-    updateCartPro();
-    updateNumber(); 
-  })
+searchBar.addEventListener("input", () => {
+  updateFilteredProducts();
+  updateExtraPro();
+  updateCartPro();
+  updateNumber();
+});
 
 updateFilteredProducts();
 updateNumber();
 updateCartPro();
 updateExtraPro();
-const productHTML=document.querySelectorAll(".js-Product");
-localStorage.setItem("extraItems",JSON.stringify(extraProducts));
-localStorage.setItem("cartItems",JSON.stringify(cartProducts));
+const productHTML = document.querySelectorAll(".js-Product");
+localStorage.setItem("extraItems", JSON.stringify(extraProducts));
+localStorage.setItem("cartItems", JSON.stringify(cartProducts));
--- JavaScript/checkoutScript.js
+++ JavaScript/checkoutScript.js
@@ -1,78 +1,118 @@
-const checkoutItems=JSON.parse(localStorage.getItem("cartItems"))||[];
-const checkOutProducts=document.querySelector(".js-SummaryProducts");
-const summary=document.querySelector(".js-Summary");
-class Product{
-  id;
-  name;
-  description;
-  price;
-  image;
-  constructor(id,name,description,price,image){
-    this.id=id;
-    this.name=name;
-    this.description=description;
-    this.price=price;
-    this.image=image;
-  };
-}
-const products=[
-  new Product(1,"Amazon Astro","Household robot for home monitoring, with Alexa.",20.99,"../images/RobotProduct.jpeg"),
-
-  new Product(2,"Echo & Alexa Devices","Play music, control your smart home, get information, and more using just your voice.",17.45,"../images/alexaDevice.jpg"),
-
-  new Product(3,"Smart Home","Create a smart and connected home with Alexa and products from Amazon.",14.13,"../images/tabletProduct.jpg"),
-
-  new Product(4,"Amazon Kids","Devices and content designed just for kids to safely learn, grow and explore.",30.87,"../images/kidsProduct.png"), 
-  
-  new Product(5,"Kindle E-readers","The best devices for reading and writing, period. Hold thousands of books with no screen glare.",25.32,"../images/notbookProduct.png"),
-
-  new Product(6,"Home Security from Amazon","Protect your entire home with security systems and cameras from Amazon.",32.12,"../images/speakerProduct.jpeg"),
-
-  new Product(7,"Fire TV","Stream over 1.5 Million TV episodes and movies. Watch favorites from Netflix, Prime Video, Disney+, Max , and more.",57.45,"../images/fireTv.png"),
-  
-  new Product(8,"Fire Tablets","Powerful tablets designed for entertainment—at an affordable price.",21.75,"../images/tablets.jpeg"),
-   
-  new Product(9,"Amazon Luna","Play games like you stream movies. Luna is a cloud gaming service that lets you play games on devices you already own.",56.39,"../images/controller.jpeg"),
+const checkoutItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+const checkOutProducts = document.querySelector(".js-SummaryProducts");
+const summary = document.querySelector(".js-Summary");
+const products = [
+  {
+    id: 1,
+    name: "Amazon Astro",
+    description: "Household robot for home monitoring, with Alexa.",
+    price: 20.99,
+    image: "../images/RobotProduct.jpeg",
+  },
+  {
+    id: 2,
+    name: "Echo & Alexa Devices",
+    description:
+      "Play music, control your smart home, get information, and more using just your voice.",
+    price: 17.45,
+    image: "../images/alexaDevice.jpg",
+  },
+  {
+    id: 3,
+    name: "Smart Home",
+    description:
+      "Create a smart and connected home with Alexa and products from Amazon.",
+    price: 14.13,
+    image: "../images/tabletProduct.jpg",
+  },
+  {
+    id: 4,
+    name: "Amazon Kids",
+    description:
+      "Devices and content designed just for kids to safely learn, grow and explore.",
+    price: 30.87,
+    image: "../images/kidsProduct.png",
+  },
+  {
+    id: 5,
+    name: "Kindle E-readers",
+    description:
+      "The best devices for reading and writing, period. Hold thousands of books with no screen glare.",
+    price: 25.32,
+    image: "../images/notbookProduct.png",
+  },
+  {
+    id: 6,
+    name: "Home Security from Amazon",
+    description:
+      "Protect your entire home with security systems and cameras from Amazon.",
+    price: 32.12,
+    image: "../images/speakerProduct.jpeg",
+  },
+  {
+    id: 7,
+    name: "Fire TV",
+    description:
+      "Stream over 1.5 Million TV episodes and movies. Watch favorites from Netflix, Prime Video, Disney+, Max , and more.",
+    price: 57.45,
+    image: "../images/fireTv.png",
+  },
+  {
+    id: 8,
+    name: "Fire Tablets",
+    description:
+      "Powerful tablets designed for entertainment—at an affordable price.",
+    price: 21.75,
+    image: "../images/tablets.jpeg",
+  },
+  {
+    id: 9,
+    name: "Amazon Luna",
+    description:
+      "Play games like you stream movies. Luna is a cloud gaming service that lets you play games on devices you already own.",
+    price: 56.39,
+    image: "../images/controller.jpeg",
+  },
 ];
-let extraItems=[];
-function updateExtraItems(){
-extraItems=products.filter((product,index)=>{
-  let include=false; 
-  checkoutItems.forEach((item,index)=>{
-    if(item.id===product.id){
-      include=true;
+let extraItems = [];
+function updateExtraItems() {
+  extraItems = products.filter((product, index) => {
+    let include = false;
+    checkoutItems.forEach((item, index) => {
+      if (item.id === product.id) {
+        include = true;
+      }
+    });
+    if (!include) {
+      return product;
     }
   });
-  if(!include){
-    return product;
-  }
-});
 }
 updateExtraItems();
-let items=0;
-let price=0;
-let totalShipping=0;
-let choosePrice=[];
-const date=[
+let items = 0;
+let price = 0;
+let totalShipping = 0;
+let choosePrice = [];
+const date = [
   {
-    day:`${dayjs().add(7,"day").format("dddd, MMMM D")}`,
-    price:0.00
+    day: `${dayjs().add(7, "day").format("dddd, MMMM D")}`,
+    price: 0.0,
   },
   {
-    day:`${dayjs().add(4,"day").format("dddd, MMMM D")}`,
-    price:4.99
+    day: `${dayjs().add(4, "day").format("dddd, MMMM D")}`,
+    price: 4.99,
   },
   {
-    day:`${dayjs().add(1,"day").format("dddd, MMMM D")}`,
-    price:9.99
-  }
+    day: `${dayjs().add(1, "day").format("dddd, MMMM D")}`,
+    price: 9.99,
+  },
 ];
-function updateCheckoutItems(){
-let checkOutProductsHTML="";
-items=0;
-price=0;
-checkoutItems.forEach((item,index)=>{
-  checkOutProductsHTML+=`
+function updateCheckoutItems() {
+  let checkOutProductsHTML = "";
+  items = 0;
+  price = 0;
+  checkoutItems.forEach((item, index) => {
+    checkOutProductsHTML += `
   <div class="summaryProduct">
   <h3 class="js-DeliveryDate">
     Delivery date: ${date[0].day}
@@ -112,38 +152,39 @@ checkoutItems.forEach((item,index)=>{
 </div>
 </div>
 </div>
-`
+`;
 
-items+=1;
-price=((item.price*100)+(price*100))/100;
-});
-checkOutProducts.innerHTML=checkOutProductsHTML;
-updateSummary();
+    items += 1;
+    price = (item.price * 100 + price * 100) / 100;
+  });
+  checkOutProducts.innerHTML = checkOutProductsHTML;
+  updateSummary();
 
-const removeButtons = document.querySelectorAll(".js-RemoveItem");
-  removeButtons.forEach((button,index)=>{
-   button.addEventListener("click",()=>{
-     checkoutItems.splice(index,1);
-     localStorage.setItem("cartItems",JSON.stringify(checkoutItems));
-     updateCheckoutItems();
-     updateNumber();
-     updateExtraItems();
-     checkNumber();
-     localStorage.setItem('extraItems',JSON.stringify(extraItems));
-   }
+  const removeButtons = document.querySelectorAll(".js-RemoveItem");
+  removeButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      checkoutItems.splice(index, 1);
+      localStorage.setItem("cartItems", JSON.stringify(checkoutItems));
+      updateCheckoutItems();
+      updateNumber();
+      updateExtraItems();
+      checkNumber();
+      localStorage.setItem("extraItems", JSON.stringify(extraItems));
+    });
+  });
+}
+function updateSummary() {
+  totalShipping = 0;
+  choosePrice.forEach((cPrice, index) => {
+    totalShipping = parseFloat(
+      ((cPrice * 100 + totalShipping * 100) / 100).toFixed(2)
+    );
+  });
+  let beforeTax = parseFloat(
+    ((price * 100 + totalShipping * 100) / 100).toFixed(2)
   );
-  }
-);
-};
-function updateSummary(){
-  totalShipping=0;
-  choosePrice.forEach((cPrice,index)=>{
-    totalShipping=parseFloat((((cPrice*100)+(totalShipping*100))/100).toFixed(2));
-  }
-);
-let beforeTax=parseFloat((((price*100)+(totalShipping*100))/100).toFixed(2));
-let tax=parseFloat((((price*100)+(totalShipping*100))/1000).toFixed(2));
-summary.innerHTML=`
+  let tax = parseFloat(((price * 100 + totalShipping * 100) / 1000).toFixed(2));
+  summary.innerHTML = `
 <h3>Order Summary</h3>
 <div class="entireSummary">
 <div class="infoSummary">
@@ -161,64 +202,61 @@ summary.innerHTML=`
 </div>
 <div class="totalSummary">
 <p>Order total:</p>
-<p>$${(((beforeTax*100)+(tax*100))/100).toFixed(2)}</p>
+<p>$${((beforeTax * 100 + tax * 100) / 100).toFixed(2)}</p>
 </div>
 <button class="js-OrderButton">Place Your Order</button>
-`
-order();
+`;
+  order();
 }
 updateCheckoutItems();
-const DeliveryDate=document.querySelectorAll(".js-DeliveryDate");
-function updateDeleveryTime(){
-checkoutItems.forEach((item,index)=>{
-const chooseDate=document.querySelectorAll(`input[name="date${index}"]`);
-chooseDate.forEach((button)=>{
-  button.addEventListener("change",(event)=>{
-    DeliveryDate[index].innerHTML=("Delivery date: "+event.target.value);
-    if(event.target.value===date[0].day){
-      choosePrice[index]=0.00;
-      updateSummary();
-    }
-    else if(event.target.value===date[1].day){
-      choosePrice[index]=4.99;
-      updateSummary();
-    }
-    else if(event.target.value===date[2].day){
-      choosePrice[index]=9.99;
-      updateSummary();
-    }
-  }
-);
+const DeliveryDate = document.querySelectorAll(".js-DeliveryDate");
+function updateDeleveryTime() {
+  checkoutItems.forEach((item, index) => {
+    const chooseDate = document.querySelectorAll(`input[name="date${index}"]`);
+    chooseDate.forEach((button) => {
+      button.addEventListener("change", (event) => {
+        DeliveryDate[index].innerHTML = "Delivery date: " + event.target.value;
+        if (event.target.value === date[0].day) {
+          choosePrice[index] = 0.0;
+          updateSummary();
+        } else if (event.target.value === date[1].day) {
+          choosePrice[index] = 4.99;
+          updateSummary();
+        } else if (event.target.value === date[2].day) {
+          choosePrice[index] = 9.99;
+          updateSummary();
+        }
+      });
+    });
+  });
 }
-)}
-);};
-function order(){
-const OrderButton=document.querySelector(".js-OrderButton");
-OrderButton.addEventListener("click",()=>{
-localStorage.clear();
-window.location.href="Thanks.html";
-})
-};
-const ItemsInCart=document.querySelector(".js-NumberCart");
+function order() {
+  const OrderButton = document.querySelector(".js-OrderButton");
+  OrderButton.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.href = "Thanks.html";
+  });
+}
+const ItemsInCart = document.querySelector(".js-NumberCart");
 
-function checkNumber(){
-if(checkoutItems.length===1){
-  checkOutProducts.classList.add("js-ProductsBottom");
-};
-if(checkoutItems.length===0){
-  document.body.innerHTML=`<div class="Empty">
+function checkNumber() {
+  if (checkoutItems.length === 1) {
+    checkOutProducts.classList.add("js-ProductsBottom");
+  }
+  if (checkoutItems.length === 0) {
+    document.body.innerHTML = `<div class="Empty">
   <h2>Your Cart Is Unfortunately Empy</h2>
   <h3>Would you Like To Add Items To It?</h3>
   <a href="index.html">
   <button>Go Back To Home Page</button>
   </a>
-  </div>`
-}
+  </div>`;
+  }
 }
 checkNumber();
 function updateNumber() {
-  ItemsInCart.innerHTML=checkoutItems.length;  
+  ItemsInCart.innerHTML = checkoutItems.length;
 }
 updateNumber();
 updateDeleveryTime();
-localStorage.setItem("cartItems",JSON.stringify(checkoutItems));
+localStorage.setItem("cartItems", JSON.stringify(checkoutItems));
